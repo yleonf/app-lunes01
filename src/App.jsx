7ed2f3448ec28eb58a0a7600343be0fdb415a48d
yasna <yleon@gmail.com>
2024-06-10 13:09:31 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Saludo from './Saludo.jsx'
+// Uso de props
+function App() {
+  return (
+    <>
+      <h1>APP REact</h1>
+      <h1>Hello</h1>
+      <Saludo nombre="Juan"/>
+    </>
+  
+  )
+}
+
+export default App
---
+++ src/Saludo.jsx
@@ -0,0 +1,7 @@
+function Saludo(props){
+    return(
+        <h1> Holaaaaa!   {props.nombre} </h1>
+    ) 
+}
+
+export default Saludo;
